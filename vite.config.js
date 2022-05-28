a4ca8d01b060d259e5354647b951d71a9a219640
Tom L <toml@example.com>
2022-05-28 18:19:37 +0100

    Tidy deps

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,4 @@
 import { defineConfig } from 'vite';
-import { importAssertionsPlugin } from 'rollup-plugin-import-assert';
-import { importAssertions } from 'acorn-import-assertions';
-import fs from 'fs';
 
 export default defineConfig({
-    // server: {
-    //     https: {
-    //         key: fs.readFileSync('./.ssl/localhost.key'),
-    //         cert: fs.readFileSync('./.ssl/localhost.pem'),
-    //     },
-    // },
-    build: {
-        rollupOptions: {
-            acornInjectPlugins: [ importAssertions ],
-            plugins: [ importAssertionsPlugin() ]
-        }
-    }
 })
